8edb4e35abc22f1b00ab6467fdd36dca54dca509
nikolaounkl <nikolaounkl@gmail.com>
2019-09-30 15:23:21 +0200

    Removed roster section

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -22,22 +22,7 @@ const Teams = () => {
 					))}
 				</div>
 			</div>
-			{/* Selected Team's Roster */}
-			<div className="conference">
-				<img src="img/west.png" alt="west" height="150" />
 
-				<div className="team-wrapper">
-					{west.map(team => (
-						<div key={uuid()} className="team smooth">
-							<img
-								src={`img/${team.abbreviation}.png`}
-								alt={`${team.abbreviation}`}
-								height="100"
-							/>
-						</div>
-					))}
-				</div>
-			</div>
 			{/* EAST Conference */}
 			<div className="conference">
 				<img src="img/east.png" alt="east" height="150" />
