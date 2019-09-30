02a416856691418983c47a04960a52aa05072a54
nikolaounkl <nikolaounkl@gmail.com>
2019-09-30 15:18:40 +0200

    Updates

---
+++ precache-manifest.c137edabb9f1ca4b4ffb02297f57f9f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a4089fe1b12ac32d965ecb3f7fd6c0",
+    "url": "/berlinba/index.html"
+  },
+  {
+    "revision": "b0ad154f20989202070e",
+    "url": "/berlinba/static/css/main.970c859c.chunk.css"
+  },
+  {
+    "revision": "e529c9829cb67d243ecc",
+    "url": "/berlinba/static/js/2.6c7a7ab7.chunk.js"
+  },
+  {
+    "revision": "b0ad154f20989202070e",
+    "url": "/berlinba/static/js/main.4cd11ab5.chunk.js"
+  },
+  {
+    "revision": "f19140b62ee1a34fcc5d",
+    "url": "/berlinba/static/js/runtime~main.3de86cc9.js"
+  }
+]);
