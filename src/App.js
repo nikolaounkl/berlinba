8f8c6f3054ba33526e13592512a7717b4b0f77df
nikolaounkl <nikolaounkl@gmail.com>
2019-10-28 17:09:07 +0100

    Added global styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@ import React from "react";
 import "./App.css";
 import TeamContextProvider from "./contexts/TeamContext";
 import Teams from "./components/Teams";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+			"Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+			"Helvetica Neue", sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: white;
+		min-height: 100vh;
+		background: url("img/nba_bg.jpg") center center;
+		background-attachment: fixed;
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+`;
 
 function App() {
 	return (
 		<div className="App">
 			<TeamContextProvider>
+				<GlobalStyle />
 				<Teams />
 			</TeamContextProvider>
 		</div>
