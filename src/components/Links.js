711a373f9ef9973e1079ad79854a4246421dd108
nikolaounkl <nikos.nikolaou.dev@gmail.com>
2019-10-31 16:38:56 +0100

    Updated playlist link

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,38 +3,38 @@ import { FaYoutube, FaChartBar, FaUsers } from "react-icons/fa";
 import { GiTrophy } from "react-icons/gi";
 
 const Links = () => {
-    return (
-        <div className="links">
-            <a
-                href="https://www.youtube.com/watch?v=RzhTiAq5IrU&list=PL5j8RirTTnK6SwgRfls1MbJqCFgmxRu-3&index=1"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <FaYoutube className="icon smooth" />
-            </a>
-            <a
-                href="https://www.nba.com/standings"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <GiTrophy className="icon smooth" />
-            </a>
-            <a
-                href="https://stats.nba.com/teams/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <FaChartBar className="icon smooth" />
-            </a>
-            <a
-                href="https://stats.nba.com/players/"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <FaUsers className="icon smooth" />
-            </a>
-        </div>
-    );
+	return (
+		<div className="links">
+			<a
+				href="https://www.youtube.com/playlist?list=PL5j8RirTTnK6SwgRfls1MbJqCFgmxRu-3"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FaYoutube className="icon smooth" />
+			</a>
+			<a
+				href="https://www.nba.com/standings"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<GiTrophy className="icon smooth" />
+			</a>
+			<a
+				href="https://stats.nba.com/teams/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FaChartBar className="icon smooth" />
+			</a>
+			<a
+				href="https://stats.nba.com/players/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FaUsers className="icon smooth" />
+			</a>
+		</div>
+	);
 };
 
 export default Links;
