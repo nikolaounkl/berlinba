87d90b8fbadba5c299c87c627d03f553972bea38
nikolaounkl <nikos.nikolaou.dev@gmail.com>
2019-10-31 16:40:33 +0100

    Updates

---
+++ precache-manifest.d1cf13c10eed0612296205a95b345cb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e65d6850d9e65ce6cdb2bf650bf0b8",
+    "url": "/berlinba/index.html"
+  },
+  {
+    "revision": "9ebeee3d34d4ba867f0f",
+    "url": "/berlinba/static/css/main.9529cc3e.chunk.css"
+  },
+  {
+    "revision": "ead4ee1d02916025d9fa",
+    "url": "/berlinba/static/js/2.fed5f05e.chunk.js"
+  },
+  {
+    "revision": "9ebeee3d34d4ba867f0f",
+    "url": "/berlinba/static/js/main.ed7a9e46.chunk.js"
+  },
+  {
+    "revision": "f19140b62ee1a34fcc5d",
+    "url": "/berlinba/static/js/runtime~main.3de86cc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/berlinba/precache-manifest.c1ede9aed404c327b7b3a54fd1b12a22.js"
+  "/berlinba/precache-manifest.d1cf13c10eed0612296205a95b345cb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/berlinba/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
